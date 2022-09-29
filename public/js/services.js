e807409e470a58237efabae2f78081e1f3950ba2
NormanBetancourth <norman.betancourtt.mairena@est.una.ac.cr>
2022-09-29 02:25:20 -0600

    implementando FP_TOOKS

--- public/js/services.js
+++ public/js/services.js
@@ -6,7 +6,6 @@
 */
 
 //Tools para filtros
-//Tools para filtros
 const not = f => p => !f(p)
 const True = () => true
 const False = not(True)
@@ -17,18 +16,37 @@ const or = (...filters) => filters.reduce(or2, filters.length > 0 ? False : True
 
 
 
-//Filtros planos
+//Filtros custom
+const filtros = new Map()
 
 const male = p => p?.gender === 'M'
 const female = not(male)
 
-const child = p => p?.age >= 18
-const tenager = p => p?.age >= 18
-const adult = p => p?.age >= 18
-const senior = p => p?.age >= 18
+const child = p => p?.age >= 1 && p?.age == 11
+const teenager = p => p?.age >= 12 && p?.age <= 20
+const adult = p => p?.age >= 21 && p?.age <= 64
+const senior = p => p?.age > 64
+const idIs = (p, id) => p?.id == id || id === ''
+const all = () => true
 
-const startsLetterS = p => p?.firstname.startsWith('S')
 
+filtros.set('male', male)
+filtros.set('female', female)
+filtros.set('child', child)
+filtros.set('teenager', teenager)
+filtros.set('adult', adult)
+filtros.set('senior', senior)
+filtros.set('id', idIs)
+filtros.set('all', all)
+
+function filterBuilder(tipo){
+  return filtros.get(tipo)
+}
+
+
+// function andBuilder(options){
+//   return and([filterBuilder()])
+// }
 
 const res = await fetch("./../../persons.json");
 let personsJson = await res.json();
@@ -90,59 +108,19 @@ const persons = personsJson.persons.map((person) => {
   );
 });
 
-export function get_persons(url = "/person", delay = 0) {
-  return new Promise((then) =>
-    setTimeout(() =>
-      then(JSON.stringify(persons.map((p) => p.toObj())), (delay % 1000) * 1000)
-    )
-  );
-}
 
-function filterBuilder(tipo, p = {id: -1, firstname: '', lastname: '', age: 0, gender: 'M'}){
+export function get_persons(url = "/person", delay = 0, options = {}) {
+  const age = filterBuilder(options.age)
+  const gender =  filterBuilder(options.gender)
+  const id = filterBuilder('id')
 
-
-  // x?.P 
-  switch(tipo){
-    case 'id':
-      return ((p, id) => id == '' ||  p?.id == id);
-    case 'gender':
-      return ( (p, {gender}) => gender == p?.gender);
-    case 'age':
-      return (({age}, {ageRange}) => age <= ageRange?.max && age >= ageRange?.min)
-
-  }
- 
-
-
-}
-
-// console.log('>>>1',persons.filter(and(male,adult,startsWithLetterS)))
-// console.log('>>>2',persons.filter(and(adult,startsWithLetterS)))
-// console.log('>>>3',persons.filter(and()))
-// console.log('>>>4',persons.filter(or2(male,startsWithLetterS)))
-// console.log('>>>5',persons.filter(or(male,startsWithLetterJ, startsWithLetterS)))
-// console.log('>>>6',persons.filter(or()))
-
-
-export function getPersonById(url = "/person", id, arr, delay = 0) {
-
-    return new Promise((then) =>
-      setTimeout(() =>
-        then(JSON.stringify(arr.filter(e => filterBuilder('id')(e, id)))), (delay % 1000) * 1000)
-    );
-}
-
-
-export function getPersonsBySelection(url = "/person", querryOptions, delay = 0, arr) {
-    
-  const filterGender = querryOptions.gender == 'ALL'? (() => true): filterBuilder('gender')
-  const filterAge = querryOptions.ageRange.min  == 'ALL'? (() => true): filterBuilder('age')
 
   return new Promise((then) =>
     setTimeout(() =>
-      then(JSON.stringify(arr.filter( p => filterGender(p, querryOptions))
-      .filter((p) => filterAge(p, querryOptions))
-      ), (delay % 1000) * 1000)
+      then(JSON.stringify(
+        persons.map((p) => p.toObj()).filter( and(age, gender) ).filter(e =>  id(e, options.id))
+        
+        ), (delay % 1000) * 1000)
     )
   );
 }
